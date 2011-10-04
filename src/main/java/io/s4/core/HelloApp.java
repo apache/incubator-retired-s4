68a24d9b125fdfbab3fd9cb6d5692d56148f5b04
Leo Neumeyer <leo@s4.io>
2011-10-03 23:23:01 -0700

    Load HelloApp class using JBOSS Modules.

---
+++ src/main/java/io/s4/core/HelloApp.java
@@ -0,0 +1,9 @@
+package io.s4.core;
+
+public class HelloApp {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Hello App!");
+    }
+}
---
+++ src/main/java/io/s4/core/Main.java
@@ -0,0 +1,34 @@
+package io.s4.core;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+
+public class Main {
+
+    /**
+     * Starts an S4 server.
+     * 
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        final Logger logger = LoggerFactory.getLogger(Main.class);
+
+        /*
+         * Need to get name of plugin module. Load ControllerModule to get
+         * configuration.
+         */
+        Injector injector = Guice.createInjector(new io.s4.core.Module());
+
+        Controller controller = injector.getInstance(Controller.class);
+        try {
+            controller.start();
+        } catch (Exception e) {
+            logger.error("Failed to start the controller.", e);
+        }
+
+    }
+}
