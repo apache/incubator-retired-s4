a6b382d28ce1309ab64878a8a5d27f636960feae
Leo Neumeyer <leo@s4.io>
2011-10-23 18:39:35 -0700

    Read MANIFEST main attributes, get teh S4 app name attribute and create a new instance.

--- subprojects/s4-core/src/main/java/org/apache/s4/core/Server.java
+++ subprojects/s4-core/src/main/java/org/apache/s4/core/Server.java
@@ -1,7 +1,9 @@
 package org.apache.s4.core;
 
 import java.net.URL;
-
+import java.util.jar.Attributes;
+import java.util.jar.JarFile;
+import java.util.jar.Manifest;
 
 import org.apache.s4.base.util.JarLoader;
 import org.slf4j.Logger;
@@ -21,7 +23,7 @@ import com.google.inject.name.Named;
  * layer.
  */
 public class Server {
-    
+
     final private String moduleName;
     final private String logLevel;
 
@@ -59,30 +61,39 @@ public class Server {
 
         // HERE WE SHOULD LOOP TO CHECK IF WE NEED TO LOAD OR UNLOAD APPS.
 
-
-        // MAKE SURE YOU COPY THE RESOURCE TO THE CLASSPATH 
+        // MAKE SURE YOU COPY THE RESOURCE TO THE CLASSPATH
         // example: subprojects/s4-core/bin/apps/MY_RESOURCE (in Eclipse)
-        //String resource = "/apps/HelloApp.jar";
+        // String resource = "/apps/HelloApp.jar";
         String resource = "/apps/CounterExample.s4r";
         // Read the jar as a resource into a URL.
         URL url = this.getClass().getResource(resource);
-        if(url == null) {
+        if (url == null) {
             logger.error("Couldn't read resource.");
             System.exit(-1);
         }
         logger.trace("Read: {}", url.toString());
-        
-        // Convert the URL to a File and load the jar.
-        JarLoader cl = new JarLoader(url.getFile());
-        
-//        URL url = this.getClass().getResource("/apps/HelloApp.jar");
-//        logger.trace("URL: " + url.toString());
-        
-        // LOOK AT the custom classloader class MultiClassLoader, for now it
 
-        String appClassName = "org.apache.s4.example.counter.MyApp"; // THE APP CLASS WE NEED TO GET FROM THE MANIFEST.
-        App myApp=null;
+        /* Convert the URL to a File and load the jar. */
+        JarLoader cl = new JarLoader(url.getFile());
+
+        /* Read MANIFEST main attributes. We need the name of the App class. */
+        String appClassName="";
+        try {
+            JarFile jar = new JarFile(url.getFile());
+            Manifest manifest = jar.getManifest();
+            Attributes attributes = manifest.getMainAttributes();
+            for (Object name : attributes.keySet()) {
+                logger.debug(name + ": " + attributes.getValue((Attributes.Name)name));
+            }
+            appClassName = attributes.getValue("S4-App-Class");
+        } catch (Exception e) {
+           logger.error(e.getMessage(), e);
+        }
         
+        logger.info("Loading application class: " + appClassName);
+        App myApp = null;
+
+        /* Create app. App must have a zero-arg constructor. */
         try {
             Object o = (cl.loadClass(appClassName)).newInstance();
             myApp = (App) o;
@@ -90,11 +101,13 @@ public class Server {
             System.out.println("Caught exception : " + e);
             e.printStackTrace();
         }
-        
+
+        /* Set up app and call life-cycle methods. */
         Sender sender = injector.getInstance(Sender.class);
         Receiver receiver = injector.getInstance(Receiver.class);
         myApp.setCommLayer(sender, receiver);
         myApp.init();
         myApp.start();
+        myApp.close();
     }
 }
