44e354642ff5af28174d44e16bb90f8902986f80
Matthieu Morel <mmorel@apache.org>
2012-07-24 10:25:40 +0200

    Update license headers in all java and gradle files

--- subprojects/s4-core/src/main/java/org/apache/s4/core/ft/SaveStateTask.java
+++ subprojects/s4-core/src/main/java/org/apache/s4/core/ft/SaveStateTask.java
@@ -15,6 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.apache.s4.core.ft;
 
 import java.util.concurrent.ExecutionException;
