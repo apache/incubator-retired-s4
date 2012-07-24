81ef1e3944d327ef53a1912ab7ab403a1bdf91a3
Matthieu Morel <mmorel@apache.org>
2012-07-24 19:40:15 +0200

    Update license headers after rebase on top of latest changes in piper branch

--- subprojects/s4-core/src/main/java/org/apache/s4/core/window/package-info.java
+++ subprojects/s4-core/src/main/java/org/apache/s4/core/window/package-info.java
@@ -16,26 +16,10 @@
  * limitations under the License.
  */
 
-package org.apache.s4.core.window;
-
 /**
- * A convenience window slot, that aggregates elements of type <T>.
+ * Provides facilities for processing events as samples, through time or count-based windows. 
+ * Currently we only provide a facility for sliding windows.
  * 
- * Users must add suitable getter methods to retrieve aggregated data.
- * 
- * @param <T>
- *            elements to aggregate
+ * NOTE: we are still working on improving the API here.
  */
-public interface Slot<T> {
-
-    /**
-     * Add a single data element
-     */
-    void update(T data);
-
-    /**
-     * Compute aggregated data on available gathered slot data, place slot and slot data in immutable state.
-     */
-    void close();
-
-}
+package org.apache.s4.core.window;
