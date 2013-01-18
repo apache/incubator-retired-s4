2ecb531f7a641ec7751299a721cc1004720a4c5f
Matthieu Morel <mmorel@apache.org>
2013-01-18 16:50:27 +0100

    Improve application and platform configuration - added missing files

---
+++ test-apps/custom-modules/src/main/java/org/apache/s4/TestListener.java
@@ -0,0 +1,136 @@
+package org.apache.s4;
+
+import java.net.InetSocketAddress;
+import java.util.concurrent.Executors;
+
+import org.apache.commons.lang.NotImplementedException;
+import org.apache.s4.base.Event;
+import org.apache.s4.base.EventMessage;
+import org.apache.s4.base.Listener;
+import org.apache.s4.base.SerializerDeserializer;
+import org.apache.s4.comm.topology.Assignment;
+import org.apache.s4.comm.topology.ClusterNode;
+import org.apache.s4.comm.topology.ZkClient;
+import org.jboss.netty.bootstrap.ServerBootstrap;
+import org.jboss.netty.buffer.ChannelBuffer;
+import org.jboss.netty.channel.Channel;
+import org.jboss.netty.channel.ChannelFactory;
+import org.jboss.netty.channel.ChannelFuture;
+import org.jboss.netty.channel.ChannelFutureListener;
+import org.jboss.netty.channel.ChannelHandlerContext;
+import org.jboss.netty.channel.ChannelPipeline;
+import org.jboss.netty.channel.ChannelPipelineFactory;
+import org.jboss.netty.channel.Channels;
+import org.jboss.netty.channel.ExceptionEvent;
+import org.jboss.netty.channel.MessageEvent;
+import org.jboss.netty.channel.SimpleChannelHandler;
+import org.jboss.netty.channel.group.ChannelGroup;
+import org.jboss.netty.channel.group.DefaultChannelGroup;
+import org.jboss.netty.channel.socket.nio.NioServerSocketChannelFactory;
+import org.jboss.netty.handler.codec.frame.LengthFieldBasedFrameDecoder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.inject.Inject;
+import com.google.inject.name.Named;
+
+public class TestListener implements Listener {
+
+    private static Logger logger = LoggerFactory.getLogger(TestListener.class);
+
+    private ServerBootstrap bootstrap;
+    private final ChannelGroup channels = new DefaultChannelGroup();
+    private ZkClient zkClient;
+    private SerializerDeserializer serDeser;
+
+    @Inject
+    public TestListener(Assignment assignment, @Named("s4.comm.timeout") int timeout, SerializerDeserializer serDeser) {
+        // wait for an assignment
+        ClusterNode node = assignment.assignClusterNode();
+        this.serDeser = serDeser;
+
+        ChannelFactory factory = new NioServerSocketChannelFactory(Executors.newCachedThreadPool(),
+                Executors.newCachedThreadPool());
+
+        bootstrap = new ServerBootstrap(factory);
+
+        bootstrap.setPipelineFactory(new ChannelPipelineFactory() {
+            @Override
+            public ChannelPipeline getPipeline() {
+                ChannelPipeline p = Channels.pipeline();
+                p.addLast("1", new LengthFieldBasedFrameDecoder(999999, 0, 4, 0, 4));
+                p.addLast("2", new MyChannelHandler());
+
+                return p;
+            }
+        });
+
+        bootstrap.setOption("child.tcpNoDelay", true);
+        bootstrap.setOption("child.keepAlive", true);
+        bootstrap.setOption("child.reuseAddress", true);
+        bootstrap.setOption("child.connectTimeoutMillis", timeout);
+        bootstrap.setOption("readWriteFair", true);
+
+        Channel c = bootstrap.bind(new InetSocketAddress(node.getPort()));
+        channels.add(c);
+
+        zkClient = new ZkClient("localhost:2181");
+    }
+
+    @Override
+    public byte[] recv() {
+        while (true) {
+            try {
+                Thread.sleep(100000);
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void close() {
+        try {
+            channels.close().await();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+        bootstrap.releaseExternalResources();
+
+    }
+
+    @Override
+    public int getPartitionId() {
+        throw new NotImplementedException();
+    }
+
+    public class MyChannelHandler extends SimpleChannelHandler {
+        @Override
+        public void messageReceived(ChannelHandlerContext ctx, MessageEvent e) {
+            channels.add(e.getChannel());
+            ChannelBuffer buffer = (ChannelBuffer) e.getMessage();
+            Event event = (Event) serDeser.deserialize(((EventMessage) serDeser.deserialize(buffer.array()))
+                    .getSerializedEvent());
+
+            zkClient.createEphemeralSequential("/test/data", event.get("message", long.class));
+
+        }
+
+        @Override
+        public void exceptionCaught(ChannelHandlerContext context, ExceptionEvent event) {
+            logger.error("Error", event.getCause());
+            Channel c = context.getChannel();
+            c.close().addListener(new ChannelFutureListener() {
+                @Override
+                public void operationComplete(ChannelFuture future) throws Exception {
+                    if (future.isSuccess())
+                        channels.remove(future.getChannel());
+                    else
+                        logger.error("FAILED to close channel");
+                }
+            });
+        }
+    }
+
+}
